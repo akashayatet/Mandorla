a73358dbf0c4bd9c61071ea2e39646a8e732cd4d
akashayatet <akashayatet@logaeth.org>
2021-02-14 20:16:00 -0800

    basic SKRY

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,19 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<!-- <h1>Hello {name}!</h1>
 	<p>This is a starter template for a Svelte PWA, based in the <a href="https://github.com/sveltejs/template" target="_blank">Svelte template</a></p>
-	<p>You will find the manifest.json file and the service-worker.js file in the public folder</p>
-	<canvas id="gameCanvas" width="800" height="1200"></canvas>
+	<p>You will find the manifest.json file and the service-worker.js file in the public folder</p> -->
+	<div id="boundary">
+		<canvas id="mindfield"></canvas>
+	</div>
 </main>
 
 <style>
 	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -24,8 +29,20 @@
 		font-weight: 100;
 	}
 
-	#gameCanvas {
+	#boundary {
+		border-radius: 50%;
+		background:greenyellow;
+		width: 32rem;
+		height: 32rem;
+		padding: 9px;	
+		box-shadow: 0 0 0 6px black;
+	}
+
+	#mindfield {
 		background: black;
+		border-radius: 50%;
+		width: 32rem;
+		height: 32rem;
 	}
 
 	@media (min-width: 640px) {
